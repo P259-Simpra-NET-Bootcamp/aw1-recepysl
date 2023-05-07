4f23b88a2c6dcbf7595936fa533dfb0f1c49fbb3
Recep <recepysl@gmail.com>
2023-05-07 14:24:40 +0300

    init

---
+++ SimApi/SimApi.Data/Context/SimDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimApi.Data.Domain;
+
+namespace SimApi.Data.Context;
+
+public class SimDbContext : DbContext
+{
+    public SimDbContext(DbContextOptions<SimDbContext> options) : base(options)
+    {
+
+    }
+
+    // dbset
+    public DbSet<Product> Product { get; set; }
+    public DbSet<Category> Category { get; set; }
+    public DbSet<User> User { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new ProductConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ SimApi/SimApi.Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using SimApi.Data.Domain;
+using SimApi.Data.Repository;
+
+namespace SimApi.Data.Uow;
+
+public interface IUnitOfWork : IDisposable
+{
+    IGenericRepository<Category> CategoryRepository { get; }
+    IGenericRepository<Product> ProductRepository { get; }
+
+    void Complete();
+    void CompleteWithTransaction();
+}
